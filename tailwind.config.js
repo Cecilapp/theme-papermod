ee1699ffe4fb02a83d5876355283b21d930b4842
Arnaud Ligny <arnaud@ligny.fr>
2025-02-15 04:00:55 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,33 +2,33 @@ const typographyStyles = require('./typography');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './layouts/**/*.{html,html.twig,js,twig}'
-    ],
-    theme: {
-        typography: typographyStyles,
-        container: {
-            screens: {
-                sm: '100%',
-                md: '100%',
-                lg: '1024px',
-                xl: '1024px',
-            }
-        },
-        extend: {
-            colors: {
-                yellow: {
-                    d: '#ffff00',
-                },
-                code: {
-                    pre: {
-                        bg: '#2b2b2b'
-                    }
-                }
-            },
-        },
+  content: [
+    './layouts/**/*.{html,html.twig,js,twig}'
+  ],
+  theme: {
+    typography: typographyStyles,
+    container: {
+      screens: {
+        sm: '100%',
+        md: '100%',
+        lg: '1024px',
+        xl: '1024px',
+      }
     },
-    plugins: [
-        require('@tailwindcss/typography'),
-    ]
+    extend: {
+      colors: {
+        yellow: {
+          d: '#ffff00',
+        },
+        code: {
+          pre: {
+            bg: '#2b2b2b'
+          }
+        }
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ]
 }
